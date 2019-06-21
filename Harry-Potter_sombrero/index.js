6125baf028d68d83035e2976d785af5b6f294865
villegas-06 <villegas4006@gmail.com>
2019-06-21 13:47:01 -0500

    harry_potter

---
+++ Harry-Potter_sombrero/index.js
@@ -0,0 +1,15 @@
+//Variables
+var asigancionCasa = ["G","R","S","H"];
+var nuevosEstudiantes = 16;
+var estudiantes = "";
+var casaResultado = [0,0,0,0];
+
+for(var i = 0; i < nuevosEstudiantes; i++){
+    estudiantes = prompt("elija la asignación de la casa: "
+    + "G: Gryffindor " + "R: Ravenclaw " 
+    + "S: Slytherin " + "H: Hufflepuff");
+
+    estudiantes = estudiantes.toLowerCase();
+    
+
+}
