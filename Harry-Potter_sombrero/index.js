b9c14751dc50b965e3152b29808e612484577547
villegas-06 <villegas4006@gmail.com>
2019-07-02 17:27:43 -0500

    retos conceptos

--- Harry-Potter_sombrero/index.js
+++ Harry-Potter_sombrero/index.js
@@ -4,12 +4,15 @@ var nuevosEstudiantes = 16;
 var estudiantes = "";
 var casaResultado = [0,0,0,0];
 
-for(var i = 0; i < nuevosEstudiantes; i++){
-    estudiantes = prompt("elija la asignación de la casa: "
-    + "G: Gryffindor " + "R: Ravenclaw " 
-    + "S: Slytherin " + "H: Hufflepuff");
-
-    estudiantes = estudiantes.toLowerCase();
+for(var i = 1; i < nuevosEstudiantes; i++){
+   
+    do{
+        estudiantes = prompt("elija la asignación de la casa: "
+        + "G: Gryffindor " + "R: Ravenclaw " 
+        + "S: Slytherin " + "H: Hufflepuff" + " " +  ":" + i);
+    
+        estudiantes = estudiantes.toLowerCase();
+    }while(estudiantes == "" );
     
 
 }
